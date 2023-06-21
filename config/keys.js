e747c8794a382953823f27a901f832d06c8e8272
Deepak Rosario Pancras <rozartharigopla@gmail.com>
2023-06-21 02:37:03 -0700

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports={
+    googleClientID : '866315256597-blrligsusn0rvt87u6ke5vt9fplskckr.apps.googleusercontent.com',
+    googleClientSecret:'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',
+    mongoURI: 'mongodb+srv://miknapsack:miKnapsack123!@oilstore.x5kx6sm.mongodb.net/?retryWrites=true&w=majority',
+    cookieKey: 'dkjgkdljfgkdfjgdlfjglkdfjgierotuerngmdfgnkflbjbcjbkclb'
+}
+
+//prod
+//mongoURI:'mongodb+srv://miknapsack:miKnapsack123!@knapsack.nfmaprb.mongodb.net/?retryWrites=true&w=majority'
