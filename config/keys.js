af4bf5bae2b2b92c9624eaea09036bfceaa35e13
Deepak Rosario Pancras <rozartharigopla@gmail.com>
2023-06-21 03:24:22 -0700

    finished heroku env setup

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
-module.exports={
-    googleClientID : '866315256597-blrligsusn0rvt87u6ke5vt9fplskckr.apps.googleusercontent.com',
-    googleClientSecret:'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',
-    mongoURI: 'mongodb+srv://miknapsack:miKnapsack123!@oilstore.x5kx6sm.mongodb.net/?retryWrites=true&w=majority',
-    cookieKey: 'dkjgkdljfgkdfjgdlfjglkdfjgierotuerngmdfgnkflbjbcjbkclb'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
 }
 
 //prod
